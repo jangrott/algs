3fcd7c871bf42358f2049d26a89866e7fc85befa
jangrott <tomek.jangrot@gmail.com>
2018-07-16 17:40:23 +0200

    TransposeMatrix (2)

--- src/main/java/pl/jangrot/algs/TransposeMatrix.java
+++ src/main/java/pl/jangrot/algs/TransposeMatrix.java
@@ -4,7 +4,7 @@ public class TransposeMatrix {
 
     public static int[][] transposeMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix[i].length; j++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
                 int a = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = a;
--- src/test/groovy/pl/jangrot/algs/TransposeMatrixSpec.groovy
+++ src/test/groovy/pl/jangrot/algs/TransposeMatrixSpec.groovy
@@ -8,7 +8,8 @@ class TransposeMatrixSpec extends Specification {
         expect:
         TransposeMatrix.transposeMatrix(input) == output
         where:
-        input                                         || output
-        [[1, 0] as int[], [1, 0] as int[]] as int[][] || [[1, 1] as int[], [0, 0] as int[]] as int[][]
+        input                                                                   || output
+        [[1, 0] as int[], [1, 0] as int[]] as int[][]                           || [[1, 1] as int[], [0, 0] as int[]] as int[][]
+        [[1, 2, 3] as int[], [4, 5, 6] as int[], [7, 8, 9] as int[]] as int[][] || [[1, 4, 7] as int[], [2, 5, 8] as int[], [3, 6, 9] as int[]] as int[][]
     }
 }
